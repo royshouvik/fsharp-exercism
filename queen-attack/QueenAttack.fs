f26618d338a7ad15633faf260c856d985fb34c6e
Shouvik Roy <shouvik@techie.com>
2020-02-08 20:57:49 +0530

    add queen attack

---
+++ queen-attack/QueenAttack.fs
@@ -0,0 +1,7 @@
+﻿module QueenAttack
+
+let create (position: int * int) = 
+  let x, y = position
+  x < 8 && x >= 0 && y < 8 && y >= 0
+
+let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
