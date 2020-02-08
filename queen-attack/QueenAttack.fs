a6fcb4576e46a18bafec750c3f68c5fd21c4e4b2
Zarak <zarak@hotmail.ca>
2020-02-08 20:36:16 +0500

    Pass test for same row and column

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -1,7 +1,10 @@
 ﻿module QueenAttack
 
-let create (position: int * int) = 
-  let x, y = position
-  x < 8 && x >= 0 && y < 8 && y >= 0
+let create (position: int * int) =
+    let x, y = position
+    x < 8 && x >= 0 && y < 8 && y >= 0
 
-let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
+let canAttack (queen1: int * int) (queen2: int * int) =
+    match queen1, queen2 with
+    | (x1, y1), (x2, y2) when x1 = x2 || y1 = y2 -> true // same column or same row
+    | _ -> false
