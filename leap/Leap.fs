88ca7573af275ca6adc500dfad6824650e4265b8
Shouvik Roy <shouvik@techie.com>
2020-02-15 21:30:17 +0530

    add leap year

---
+++ leap/Leap.fs
@@ -0,0 +1,8 @@
+﻿module Leap
+
+let isDivisibleBy number year = year % number = 0
+let isNotDivisibleBy number year = year % number <> 0
+
+let leapYear (year: int): bool =
+    (year |> isDivisibleBy 4 && year |> isNotDivisibleBy 100)
+    || year |> isDivisibleBy 400
