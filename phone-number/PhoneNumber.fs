ed79a94d0ba4113d92fa7147591c5547894a6515
Zarak <zarak@hotmail.ca>
2020-02-16 13:38:22 +0500

    Complete 9 digit case

--- phone-number/PhoneNumber.fs
+++ phone-number/PhoneNumber.fs
@@ -2,8 +2,12 @@
 
 open System
 
-let clean input = 
-  let number = input |> String.filter Char.IsDigit |> UInt64.TryParse
-  match number with
-  | (true, result) -> Ok result
-  | _ -> Error "go figure"
+let clean input =
+    let number = input |> String.filter Char.IsDigit
+    // |> UInt64.TryParse
+    let numberLength = String.length number
+    let maybeNumber = UInt64.TryParse number
+    match (maybeNumber, numberLength) with
+    | (true, result), 10 -> Ok result
+    | (true, result), 9 -> Error "incorrect number of digits"
+    | _ -> Error "go figure"
