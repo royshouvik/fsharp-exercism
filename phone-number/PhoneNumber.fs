4209aa786b5a26c95b0e51232f587c984ea180e3
Shouvik Roy <shouvik@techie.com>
2020-02-16 13:56:57 +0530

    phone number - first test passes

---
+++ phone-number/PhoneNumber.fs
@@ -0,0 +1,9 @@
+﻿module PhoneNumber
+
+open System
+
+let clean input = 
+  let number = input |> String.filter Char.IsDigit |> UInt64.TryParse
+  match number with
+  | (true, result) -> Ok result
+  | _ -> Error "go figure"
